145cb6979928bb207f5a00e79fdc7c663b8cb721
martin <martinnovoar2@gmail.com>
2020-07-31 02:25:28 -0300

    Added stats card

--- src/classes/Pokemon.js
+++ src/classes/Pokemon.js
@@ -1,5 +1,5 @@
 export default class Pokemon {
-  constructor(id, name, height, weight, sprites = [], types = [], skills = [], hp, attack, defence, specialAttack, specialDefence, speed) {
+  constructor(id, name, height, weight, sprites = [], types = [], skills = [], stats) {
     this.id = id;
     this.name = name;
     this.height = height;
@@ -7,11 +7,6 @@ export default class Pokemon {
     this.sprites = sprites;
     this.types = types;
     this.skills = skills;
-    this.hp = hp;
-    this.attack = attack;
-    this.defence = defence;
-    this.specialAttack = specialAttack;
-    this.specialDefence = specialDefence;
-    this.speed = speed;
+    this.stats = stats;
   }
 }

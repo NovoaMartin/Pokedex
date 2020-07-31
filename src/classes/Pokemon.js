96af56d01be03d52f5ad1334830c497a6128f812
martin <martinnovoar2@gmail.com>
2020-07-31 02:15:51 -0300

    Added general information card

--- src/classes/Pokemon.js
+++ src/classes/Pokemon.js
@@ -1,5 +1,5 @@
 export default class Pokemon {
-  constructor(id, name, height, weight, sprites = [], types = [], skills = [], stats) {
+  constructor(id, name, height, weight, sprites = [], types = [], skills = [], hp, attack, defence, specialAttack, specialDefence, speed) {
     this.id = id;
     this.name = name;
     this.height = height;
@@ -7,6 +7,11 @@ export default class Pokemon {
     this.sprites = sprites;
     this.types = types;
     this.skills = skills;
-    this.stats = stats;
+    this.hp = hp;
+    this.attack = attack;
+    this.defence = defence;
+    this.specialAttack = specialAttack;
+    this.specialDefence = specialDefence;
+    this.speed = speed;
   }
 }

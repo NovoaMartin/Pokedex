dffe8f4511b511ba76bf0b105375490004d64b62
martin <martinnovoar2@gmail.com>
2020-08-01 01:13:19 -0300

    Finished ui

---
+++ src/api/apiCalls.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+export async function getPokemonCount() {
+  return (await getPokemons(0, 1)).count;
+}
+
+export async function getPokemons(offset, limit) {
+  return (await fetch(`${BASE_URL}?offset=${offset}&limit=${limit}`)).json();
+}
+
+export async function getPokemon(id) {
+  return (await fetch(`${BASE_URL}${id}`)).json();
+}
